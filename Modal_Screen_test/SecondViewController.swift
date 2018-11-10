03dfbbc3a014cf4174ab7bfdd88cadf87d4912bc
masato <masato@m-mbp.local>
2018-11-10 14:24:59 +0800

    change from iOS UIkit Rename to Swift4

--- Modal_Screen_test/SecondViewController.swift
+++ Modal_Screen_test/SecondViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  SecondViewController.swift
 //  Modal_Screen_test
 //
 //  Created by masato on 10/11/2018.
@@ -8,40 +8,35 @@
 
 import UIKit
 
-class ViewController: UIViewController{
+class SecondViewController : UIViewController{
 
     override func viewDidLoad() {
 
-        // PopButtonを生成.
+        self.view.backgroundColor = UIColor.black
+
+        // もどるButtonを生成.
         let myButton = UIButton()
         myButton.frame = CGRect(x: 0, y: 0, width: 200, height: 40)
-        myButton.backgroundColor = .red
+        myButton.backgroundColor = UIColor.red
         myButton.layer.masksToBounds = true
-        myButton.setTitle("PopOver", for: UIControl.State.normal)
+        myButton.setTitle("もどる", for: UIControl.State.normal)
         myButton.setTitleColor(UIColor.white, for: UIControl.State.normal)
         myButton.setTitleColor(UIColor.black, for: UIControl.State.highlighted)
         myButton.layer.cornerRadius = 20.0
         myButton.layer.position = CGPoint(x: self.view.frame.width/2, y:200)
-        myButton.tag = 0
-        myButton.addTarget(self, action: #selector(ViewController.onClickMyButton(sender:)), for: .touchUpInside)
+        myButton.tag = 1
+        myButton.addTarget(self, action: #selector(SecondViewController.onClickMyButton(sender:)), for: .touchUpInside)
 
         // viewにButtonを追加.
         self.view.addSubview(myButton)
     }
 
     /*
-     Buttonが押された時に呼ばれるメソッド.
+     Buttonを押した時に呼ばれるメソッド.
      */
     @objc func onClickMyButton(sender : UIButton){
 
-        // secondViewControllerのインスタンス生成.
-        let second = SecondViewController()
-
-        // navigationControllerのrootViewControllerにsecondViewControllerをセット.
-        let nav = UINavigationController(rootViewController: second)
-
-        // 画面遷移.
-        self.present(nav, animated: true, completion: nil)
+        // viewを閉じる.
+        self.navigationController?.dismiss(animated: true, completion: nil)
     }
 }
-
